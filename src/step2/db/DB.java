6d18303628168a0ebac1a0840e8cbf5c59d0c762
flavien69 <bert.flavien@gmail.com>
2014-11-21 11:35:23 +0100

    fixe annotation of the faces

--- src/step2/db/DB.java
+++ src/step2/db/DB.java
@@ -1,68 +1,68 @@
-package step2.db;
-
-import java.sql.SQLException;
-import java.sql.Connection;
-import step2.model.UserModelBean;
-
-public class DB {
-	private static final String DB_HOST = "http://localhost/";
-	private static final String DB_PORT = "3000";
-	private static final String DB_NAME = "stepByStep";
-	private static final String DB_USER = "client";
-	private static final String DB_PWD = "client";
-	private static final String DB_TABLE_USER = "users";
-
-	private static final String LASTNAME = "lastname";
-	private static final String SURNAME = "surname";
-	private static final String AGE = "age";
-	private static final String LOGIN = "login";
-	private static final String PWD = "pwd";
-	private Connection connection;
-	
-	public DB() {
-		openConnection();
-	}
-
-	private void openConnection() {
-		try {
-			// Chargement du Driver, puis établissement de la connexion
-			Class.forName("com.mysql.jdbc.Driver");
-			// create connection
-			// connection = java.sql.DriverManager.getConnection("jdbc:mysql://"
-			// + DB_HOST + ":" + DB_PORT + "/" + DB_NAME, DB_USER, DB_PWD);
-
-			connection = java.sql.DriverManager
-					.getConnection("jdbc:mysql://localhost/" + DB_NAME + "?"
-							+ "user=" + DB_USER + "&password=" + DB_PWD);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void addUser(UserModelBean user) {
-		// Création de la requête
-		java.sql.Statement stmt;
-
-		try {
-
-			String querry = "INSERT INTO " + DB_TABLE_USER + " (" + SURNAME
-					+ "," + LASTNAME + ", " + AGE + ", " + LOGIN + ", " + PWD
-					+ ") VALUES ('" + user.getSurname() + "','"
-					+ user.getLastname() + "','" + user.getAge() + "','"
-					+ user.getLogin() + "','" + user.getPwd() + "');";
-
-			openConnection();
-			stmt = connection.createStatement();
-			stmt.executeUpdate(querry);
-
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-
-}
+//package step2.db;
+//
+//import java.sql.SQLException;
+//import java.sql.Connection;
+//import step2.model.UserModelBean;
+//
+//public class DB {
+//	private static final String DB_HOST = "http://localhost/";
+//	private static final String DB_PORT = "3000";
+//	private static final String DB_NAME = "stepByStep";
+//	private static final String DB_USER = "client";
+//	private static final String DB_PWD = "client";
+//	private static final String DB_TABLE_USER = "users";
+//
+//	private static final String LASTNAME = "lastname";
+//	private static final String SURNAME = "surname";
+//	private static final String AGE = "age";
+//	private static final String LOGIN = "login";
+//	private static final String PWD = "pwd";
+//	private Connection connection;
+//	
+//	public DB() {
+//		openConnection();
+//	}
+//
+//	private void openConnection() {
+//		try {
+//			// Chargement du Driver, puis établissement de la connexion
+//			Class.forName("com.mysql.jdbc.Driver");
+//			// create connection
+//			// connection = java.sql.DriverManager.getConnection("jdbc:mysql://"
+//			// + DB_HOST + ":" + DB_PORT + "/" + DB_NAME, DB_USER, DB_PWD);
+//
+//			connection = java.sql.DriverManager
+//					.getConnection("jdbc:mysql://localhost/" + DB_NAME + "?"
+//							+ "user=" + DB_USER + "&password=" + DB_PWD);
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	public void addUser(UserModelBean user) {
+//		// Création de la requête
+//		java.sql.Statement stmt;
+//
+//		try {
+//
+//			String querry = "INSERT INTO " + DB_TABLE_USER + " (" + SURNAME
+//					+ "," + LASTNAME + ", " + AGE + ", " + LOGIN + ", " + PWD
+//					+ ") VALUES ('" + user.getSurname() + "','"
+//					+ user.getLastname() + "','" + user.getAge() + "','"
+//					+ user.getLogin() + "','" + user.getPwd() + "');";
+//
+//			openConnection();
+//			stmt = connection.createStatement();
+//			stmt.executeUpdate(querry);
+//
+//			connection.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//
+//}
--- src/step2/servlet/Servlet3.java
+++ src/step2/servlet/Servlet3.java
@@ -1,68 +1,68 @@
-package step2.servlet;
-
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import step2.db.DB;
-import step2.model.UserModelBean;
-
-/**
- * Servlet implementation class Servlet3
- */
-@WebServlet("/Servlet3")
-public class Servlet3 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private DB db;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet3() {
-        super();
-    }
-    
-    @Override
-    public void init() throws ServletException {
-    	super.init();
-    	
-    	//Vérifie si DB existe dans l'espace de mémoire partagé entre les servlet
-    	// si oui on les récupére, si non on le crée et on l'ajoute dans l'espace de mémoire partagé entre les servlet
-    	
-    	if(getServletContext().getAttribute("BD")!=null){
-    		db=(DB)getServletContext().getAttribute("BD");
-    	}else {
-    		db=new DB();
-    		getServletContext().setAttribute("BD",db);
-    	}
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Nothing to do
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserModelBean user=(UserModelBean)request.getSession().getAttribute("myUser");
-		this.db.addUser(user);
-		//Récupération du ServletContext, création d'un dispatcher à la 
-		// destination '/follow.jsp'
-		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(
-		"/step2/display.jsp");
-
-		// Pour redirection
-		dispatch.forward(request, response);
-
-	}
-
-}
+//package step2.servlet;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import step2.db.DB;
+//import step2.model.UserModelBean;
+//
+///**
+// * Servlet implementation class Servlet3
+// */
+//@WebServlet("/Servlet3")
+//public class Servlet3 extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//	private DB db;
+//       
+//    /**
+//     * @see HttpServlet#HttpServlet()
+//     */
+//    public Servlet3() {
+//        super();
+//    }
+//    
+//    @Override
+//    public void init() throws ServletException {
+//    	super.init();
+//    	
+//    	//Vï¿½rifie si DB existe dans l'espace de mï¿½moire partagï¿½ entre les servlet
+//    	// si oui on les rï¿½cupï¿½re, si non on le crï¿½e et on l'ajoute dans l'espace de mï¿½moire partagï¿½ entre les servlet
+//    	
+//    	if(getServletContext().getAttribute("BD")!=null){
+//    		db=(DB)getServletContext().getAttribute("BD");
+//    	}else {
+//    		db=new DB();
+//    		getServletContext().setAttribute("BD",db);
+//    	}
+//    }
+//
+//	/**
+//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		//Nothing to do
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		UserModelBean user=(UserModelBean)request.getSession().getAttribute("myUser");
+//		this.db.addUser(user);
+//		//Rï¿½cupï¿½ration du ServletContext, crï¿½ation d'un dispatcher ï¿½ la 
+//		// destination '/follow.jsp'
+//		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(
+//		"/step2/display.jsp");
+//
+//		// Pour redirection
+//		dispatch.forward(request, response);
+//
+//	}
+//
+//}
